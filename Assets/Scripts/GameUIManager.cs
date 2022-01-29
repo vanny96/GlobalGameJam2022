b5313e68a222c7db5cbb41bea5122836ee34f424
Giovanni Barbaro <vanni-96@hotmail.it>
2022-01-29 17:51:29 +0100

    Fixes ghosts behaviour and client related point system

--- Assets/Scripts/GameUIManager.cs
+++ Assets/Scripts/GameUIManager.cs
@@ -7,23 +7,15 @@ using UnityEngine.UI;
 public class GameUIManager : MonoBehaviour
 {
     [SerializeField] private GameSceneManager gameSceneManager;
-
     [SerializeField] private TextMeshProUGUI treasureText;
-
     [SerializeField] private TreasureHolder playerTreasure= null;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         if (playerTreasure == null)
         {
             var playerObject = gameSceneManager.GetMyPlayerObject();
-            //Debug.Log(playerObject);
+            Debug.Log(playerObject.InputAuthority);
             if (playerObject == null) return;
         
             playerTreasure=playerObject.GetComponent<TreasureHolder>();
