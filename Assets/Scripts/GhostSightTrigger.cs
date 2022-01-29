5d24bfe786fc61aa104b448ff1596951aaafc47d
Giovanni Barbaro <vanni-96@hotmail.it>
2022-01-29 14:19:16 +0100

    Completes ghosts ai

--- Assets/Scripts/GhostSightTrigger.cs
+++ Assets/Scripts/GhostSightTrigger.cs
@@ -2,22 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TreasureHolderTrigger : MonoBehaviour
+public class GhostSightTrigger : MonoBehaviour
 {
     public TreasureHolder ActiveTreasureHolder { get; set; }
     [SerializeField] private TreasureHolder ThisTreasureHolder;
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.tag != "ActualCollider") return;
 
         TreasureHolder treasureHolder = other.GetComponentInParent<TreasureHolder>();
+        if (treasureHolder == null ||
+            treasureHolder == ThisTreasureHolder ||
+            treasureHolder == ActiveTreasureHolder)
+        {
+            return;
+        }
 
-        if (treasureHolder != null &&
-            treasureHolder != ThisTreasureHolder &&
-            ActiveTreasureHolder == null)
+        if (ActiveTreasureHolder == null)
         {
             ActiveTreasureHolder = treasureHolder;
+        }
+
+        if(ActiveTreasureHolder != null)
+        {
+            int currentTargetTreasure = ActiveTreasureHolder.treasure;
+            int newTreasureHolderTreasure = treasureHolder.treasure;
+
+            if(newTreasureHolderTreasure > currentTargetTreasure)
+            {
+                ActiveTreasureHolder = treasureHolder;
+            }
         }
     }
 
