f8ca5bebee551b0bbc1f1b108cd957b33eddb672
Giovanni Barbaro <vanni-96@hotmail.it>
2022-01-30 13:01:19 +0100

    Plays music

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -67,6 +67,7 @@ public class PlayerController : NetworkBehaviour
 
         mainSoundController = GameObject.Find("MainSoundController").GetComponent<MainSoundController>();
         gameUIManager = FindObjectOfType<GameUIManager>();
+        mainSoundController.musicPlay();
     }
 
     public override void Spawned()
@@ -135,6 +136,8 @@ public class PlayerController : NetworkBehaviour
 
     public void OnLostCoin()
     {
+        this.mainSoundController.CoinSteal();
+
         if (treasureHolder.treasure < treasureThresholdForBeacon)
         {
             isBeacon = false;
